a6a1fe1b860ccd4f755ae1688ea15b9388e968fe
phillip.buckham-bonnett@justice.gov.uk <phillip.buckham-bonnett@justice.gov.uk>
2023-10-31 23:52:04 +0000

    update code

--- render_r_code_from_source.R
+++ render_r_code_from_source.R
@@ -1,19 +1,7 @@
-# 
-# 
-# Covnert the index.Rmd content to an R script for ease of use by the participants. Only
+
+# Convert the index.Rmd content to an R script for ease of use by the participants. Only
 # code cells will be converted.
-# 
-#  
-#  
 
-
-
-library(knitr)
-
-
-purl("index.Rmd")
+knitr::purl("index.Rmd")
 
 file.rename("index.R","example_code.R")
-
-
-
